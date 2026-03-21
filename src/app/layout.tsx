70b5eab757a72d635aeedad6bb5937e233e42ec6
juakoasistente <joaquincustodiovalderas400@gmail.com>
2026-03-21 09:53:33 +0100

    chore: quitar DEMO_MODE, limpiar PNGs, iconos SVG reales
    - Eliminar bypass DEMO_MODE de middleware y layout (#42)
    - Borrar capturas praxi-*.png (#43)
    - Icono SVG profesional para PWA + favicon (#44)
    - Actualizar manifest.json con SVG icons
    
    closes #42, closes #43, closes #44

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,6 +37,7 @@ export default function RootLayout({
   return (
     <html lang="es" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/icons/icon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/icons/icon-192.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
